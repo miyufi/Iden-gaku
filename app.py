86d753d07afef81b2afff92d8c53910ac6dd7bff
miyufi <47920449+miyufi@users.noreply.github.com>
2022-06-16 23:04:55 +0800

    Added code to avoid upload build-ups

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 uploads_folder = './static/uploads'
 if not os.path.exists(uploads_folder):
     os.makedirs(uploads_folder)
@@ -32,8 +33,9 @@ def index():
     return render_template('index.html')
 
 
-@app.route("/test", methods=['POST'])
+@app.route("/upload", methods=['POST'])
 def test():
+    global target_im, target_chromosome, previous
     if 'files[]' not in request.files:
         resp = jsonify({'message': 'No file part in the request'})
         resp.status_code = 400
@@ -44,11 +46,16 @@ def test():
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        previous = filename
         success = True
     else:
         errors[file.filename] = 'File type is not allowed'
 
     if success:
+        try:
+            os.remove(os.path.join(app.config['UPLOAD_FOLDER'], previous))
+        except:
+            pass
         resp = jsonify({'message': 'File successfully uploaded'})
         resp.status_code = 201
         return resp
